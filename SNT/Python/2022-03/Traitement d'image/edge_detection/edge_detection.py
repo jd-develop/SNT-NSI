2022485902007178eb0b3aacb3bd5f8104ce262f
Jean Dubois <jd-dev@laposte.net>
2022-03-19 11:53:33 +0100

    edge detection :ok_hand:

--- SNT/Python/2022-03/Traitement d'image/edge_detection/edge_detection.py
+++ SNT/Python/2022-03/Traitement d'image/edge_detection/edge_detection.py
@@ -1,12 +1,33 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
-from PIL import Image, ImageFilter
+from PIL import Image
 from os.path import realpath
 
 input_file = realpath('../pomme.jpg')
 
 with Image.open(input_file) as image:
-    new_image = image.filter(ImageFilter.FIND_EDGES)
-    new_image.save('edge_detection_output.jpg')
-    image.paste(new_image)
-    image.save('edge_detection_output2.jpg')
+    image = image.convert("L")
+    new_image = Image.new(image.mode, image.size)
+    width, height = image.size
+    for x in range(width):
+        for y in range(height):
+            try:
+                seuil = 20
+
+                p1 = image.getpixel((x+1, y+1))
+                p2 = image.getpixel((x-1, y-1))
+                diff = abs(p1-p2)
+
+                p3 = image.getpixel((x+1, y-1))
+                p4 = image.getpixel((x-1, y+1))
+                diff2 = abs(p3-p4)
+
+                if diff > seuil or diff2 > seuil:
+                    edge = True
+                else:
+                    edge = False
+            except IndexError:
+                edge = False
+
+            new_image.putpixel((x, y), 255 if edge else 0)
+    new_image.save('edge_detection_output.png', 'PNG')
