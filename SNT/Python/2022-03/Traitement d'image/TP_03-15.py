9b3bf80f05376b5ee9c9631230e048b3917121da
Jean Dubois <jd-dev@laposte.net>
2022-03-23 11:57:39 +0100

    add comments + fix apples

--- SNT/Python/2022-03/Traitement d'image/TP_03-15.py
+++ SNT/Python/2022-03/Traitement d'image/TP_03-15.py
@@ -1,23 +1,27 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
+# ##########################################
+# INFO:
+# Il faut créer le dossier 'OUTPUT_TP_03-05' dans le répertoire courant pour que le code fonctionne.
+# ##########################################
 from PIL import Image
 
-input_file = "pomme.jpg"
+input_file = "pomme.jpg"  # chemin de l'image, en str
 
-base_img = Image.open(input_file)
-width, height = base_img.size
+base_img = Image.open(input_file)  # image qu'on ne va pas modifier (on modifie seulement ses copies)
+width, height = base_img.size  # largeur et hauteur de l'image
 
-img1 = base_img.copy()
+img1 = base_img.copy()  # on copie l'image
 r, v, b = img1.getpixel((250, 300))
 print(width, height)
 print(r, v, b)
 
 img1.putpixel((250, 250), (255, 0, 0))   # le pixel est au niveau du centre de la pomme, il est rouge
 
-img1.putpixel((100, 250), (255, 0, 0))   # le pixel est à gauche sur la pomme, il est rouge
-img1.save("./OUTPUT_TP_03-05/output1.png", "PNG")
+img1.putpixel((100, 250), (0, 0, 255))   # le pixel est à gauche sur la pomme, il est bleu
+img1.save("./OUTPUT_TP_03-05/output1.png", "PNG")  # on exporte chaque image au format png sous le nom 'output(nbr).png'
 
-# On inverse le bleu et le rouge de chaque pixel sans changer le vert.
+# Inverser le bleu et le rouge de chaque pixel sans changer le vert.
 img2 = base_img.copy()
 for y in range(height):
     for x in range(width):
@@ -26,7 +30,7 @@ for y in range(height):
 
 img2.save('./OUTPUT_TP_03-05/output2.png', 'PNG')
 
-# Mettre l'image en niveaux de gris
+# Mettre l'image en niveaux de gris avec la moyenne des couleurs
 img3 = base_img.copy()
 for y in range(height):
     for x in range(width):
@@ -36,18 +40,17 @@ for y in range(height):
 
 img3.save('./OUTPUT_TP_03-05/output3.png', 'PNG')
 
-# meilleurs niveaux de gris
+# Mettre l'image en niveaux de gris avec la méthode préconisée par
 img4 = base_img.copy()
 for y in range(height):
     for x in range(width):
         r, g, b = img4.getpixel((x, y))
-        r, g, b = r*0.2125, g*0.7154, b*0.0721
-        grey = int(r+g+b)
+        grey = int(sum((r*0.2125, g*0.7154, b*0.0721)))  # Somme de 21.25% du rouge, 71.54% du vert et 7.21% du bleu
         img4.putpixel((x, y), (grey, grey, grey))
 
 img4.save('./OUTPUT_TP_03-05/output4.png', 'PNG')
 
-# négatif
+# négatif de l'image
 img5 = base_img.copy()
 for y in range(height):
     for x in range(width):
@@ -57,7 +60,7 @@ for y in range(height):
 
 img5.save('./OUTPUT_TP_03-05/output5.png', 'PNG')
 
-# retourner l'image
+# retourner l'image horizontalement
 img6 = base_img.copy()
 new_img = Image.new(img6.mode, img6.size)
 for y in range(height):
@@ -68,7 +71,11 @@ for y in range(height):
 new_img.save('./OUTPUT_TP_03-05/output6.png', 'PNG')
 img6 = new_img
 
-# modifs persos
+# ##########################
+# Modifications personnelles
+# ##########################
+
+# je ne saurais pas décrire simplement cette modification...
 img7 = base_img.copy()
 for y in range(height):
     for x in range(width):
@@ -84,6 +91,9 @@ img8 = base_img.copy()
 for y in range(height):
     for x in range(width):
         r, b = img8.getpixel((x, y))[0], img8.getpixel((x, y))[-1]
+        # on met autant dde vert que de bleu.
+        # Ainsi, si c'est du blanc, ça reste du blanc. Mais s'il y a beaucoup de vert et peu de bleu
+        # (comme dans la pomme), cela donnera  peu de vert et peu de bleu, ce qui rend globalement rouge.
         g = b
         img8.putpixel((x, y), (r, g, b))
 
@@ -94,7 +104,7 @@ img9 = base_img.copy()
 for y in range(height):
     for x in range(width):
         r, g, b = img9.getpixel((x, y))
-        img9.putpixel((x, y), (0, g, b))
+        img9.putpixel((x, y), (0, g, b))  # on supprime le rouge de l'image
 
 img9.save('./OUTPUT_TP_03-05/output9.png', 'PNG')
 
@@ -103,7 +113,7 @@ img10 = base_img.copy()
 for y in range(height):
     for x in range(width):
         r, g, b = img10.getpixel((x, y))
-        img10.putpixel((x, y), (r, 0, b))
+        img10.putpixel((x, y), (r, 0, b))  # on supprime le vert de l'image
 
 img10.save('./OUTPUT_TP_03-05/output10.png', 'PNG')
 
@@ -112,7 +122,7 @@ img11 = base_img.copy()
 for y in range(height):
     for x in range(width):
         r, g, b = img11.getpixel((x, y))
-        img11.putpixel((x, y), (r, g, 0))
+        img11.putpixel((x, y), (r, g, 0))  # on supprime le bleu de l'image
 
 img11.save('./OUTPUT_TP_03-05/output11.png', 'PNG')
 
@@ -121,7 +131,7 @@ img12 = base_img.copy()
 for y in range(height):
     for x in range(width):
         r, g, b = img12.getpixel((x, y))
-        img12.putpixel((x, y), (255, g, b))
+        img12.putpixel((x, y), (255, g, b))  # on met le rouge à son maximum dans l'image
 
 img12.save('./OUTPUT_TP_03-05/output12.png', 'PNG')
 
@@ -130,7 +140,7 @@ img13 = base_img.copy()
 for y in range(height):
     for x in range(width):
         r, g, b = img13.getpixel((x, y))
-        img13.putpixel((x, y), (r, 255, b))
+        img13.putpixel((x, y), (r, 255, b))  # on met le vert à son maximum dans l'image
 
 img13.save('./OUTPUT_TP_03-05/output13.png', 'PNG')
 
@@ -139,11 +149,12 @@ img14 = base_img.copy()
 for y in range(height):
     for x in range(width):
         r, g, b = img14.getpixel((x, y))
-        img14.putpixel((x, y), (r, g, 255))
+        img14.putpixel((x, y), (r, g, 255))  # on met le bleu à son maximum dans l'image
 
 img14.save('./OUTPUT_TP_03-05/output14.png', 'PNG')
 
 # addition des deux autres couleurs pour chaque couleur
+# précision : si l'addition des deux autres couleurs est supérieure à 255, on met 255
 img15 = base_img.copy()
 for y in range(height):
     for x in range(width):
@@ -153,14 +164,14 @@ for y in range(height):
 
 img15.save('./OUTPUT_TP_03-05/output15.png', 'PNG')
 
-# noir et blanc
+# noir et blanc (pas de gris)
 img16 = base_img.copy()
 for y in range(height):
     for x in range(width):
         r, g, b = img16.getpixel((x, y))
-        r, g, b = r*0.2125, g*0.7154, b*0.0721
+        r, g, b = r*0.2125, g*0.7154, b*0.0721  # on met l'image en niveaux de gris
         grey = int(r+g+b)
-        if grey < 177:
+        if grey < 175:  # si le gris est inférieur à un certain seuil, on met du blanc, sinon, on met du noir
             grey = 0
         else:
             grey = 255
@@ -168,16 +179,18 @@ for y in range(height):
 
 img16.save('./OUTPUT_TP_03-05/output16.png', 'PNG')
 
-# image finale
-final_image = Image.new(base_img.mode, (500*4, 500*4))
+# image finale, à la "Andy Warhol"
+# 16 images, donc 4 images en longueur et 4 en hauteur
+final_image = Image.new(base_img.mode, (width*4, height*4))  # on fabrique une image de 4*500 sur 4*500
+# on répertorie toutes les images dans une liste
 images = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12, img13, img14, img15, img16]
-idx = 0
+idx = 0  # c'est l'index qu'on va utiliser pour parcourir la liste 'images'
 
-for y in [0, 500, 1000, 1500]:
-    for x in range(0, 500*4, 500):
-        final_image.paste(images[idx], (x, y))
-        idx += 1
-        if idx > 15:
+for y in range(0, height*4, height):  # de 0 à 500*4 de 500 en 500
+    for x in range(0, width*4, width):  # de 0 à 500*4 de 500 en 500
+        final_image.paste(images[idx], (x, y))  # on colle l'image à l'index 'idx' aux coordonnées xy
+        idx += 1  # on augmente l'index
+        if idx > len(images)-1:  # si l'index est hors de la liste, on arrête la loupe
             break
 
-final_image.save('./OUTPUT_TP_03-05/output_final.png', "PNG")
+final_image.save('./OUTPUT_TP_03-05/output_final.png', "PNG")  # on sauvegarde l'image
