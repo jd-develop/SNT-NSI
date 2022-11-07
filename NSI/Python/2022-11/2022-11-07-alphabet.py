38128db3262885efdcd1e44a584ea23820adcab0
Jean Dubois <jd-dev@laposte.net>
2022-11-07 15:52:15 +0100

    Create 2022-11-07-alphabet.py

---
+++ NSI/Python/2022-11/2022-11-07-alphabet.py
@@ -0,0 +1,9 @@
+alphabet = ''
+for i in range(26):
+    alphabet += chr(0x61 + i)
+print(alphabet)
+
+alphabet_upper = ''
+for i in range(26):
+    alphabet_upper += chr(0x41 + i)
+print(alphabet_upper)
