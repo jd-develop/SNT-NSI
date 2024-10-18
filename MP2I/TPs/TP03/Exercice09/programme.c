f9a1623d55e39b0a7c97e57ae58b1ec304c49ac3
Jean Dubois <jd-dev@laposte.net>
2024-10-18 20:11:34 +0100

    add ébauche for exercice 9

---
+++ MP2I/TPs/TP03/Exercice09/programme.c
@@ -0,0 +1,10 @@
+/*
+ * Simule une petite mémoire RAM. Ne peut stocker que des entiers. Permet
+ * d’accéder à une valeur à partir de l’adresse mémoire, de stocker une
+ * valeur dans une adresse mémoire, et de faire une addition
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <assert.h>
+
