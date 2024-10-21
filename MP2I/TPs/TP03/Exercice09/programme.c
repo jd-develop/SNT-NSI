f43970ebcb4bbffa519da7f83af6048631a99b8f
Jean Dubois <jd-dev@laposte.net>
2024-10-21 21:58:11 +0100

    tp3 : exercice9 : quiet mode

--- MP2I/TPs/TP03/Exercice09/programme.c
+++ MP2I/TPs/TP03/Exercice09/programme.c
@@ -2,11 +2,17 @@
  * Simule une petite mémoire RAM. Ne peut stocker que des entiers. Permet
  * d’accéder à une valeur à partir de son adresse mémoire, de stocker une
  * valeur à une adresse mémoire, et de faire une addition.
+ *
+ * Un mode silencieux est disponible en passant l’argument « quiet » dans la
+ * ligne de commande : pratique pour écrire des programmes en « langage
+ * machine » puis de les envoyer dans l’entrée standard :) – cf. exemple
+ * « fibo » (cat fibo | ./a.out quiet)
  */
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <assert.h>
+#include <string.h>
 
 #define MAX_LEN 1024
 
@@ -23,17 +29,21 @@
  *       lecture est à false)
  */
 char verifie_case_memoire(
-    int i, bool* deja_vu, int taille, bool lecture
+    int i, bool* deja_vu, int taille, bool lecture, bool quiet
 ) {
     if (i < 0 || i >= taille) {
-        printf("\033[31m"); // rouge
-        printf("Segmentation fault");
-        printf("\033[0m\n"); // réinitialiser la couleur
+        if (!quiet) {
+            printf("\033[31m"); // rouge
+            printf("Segmentation fault");
+            printf("\033[0m\n"); // réinitialiser la couleur
+        }
         return 1;
     } else if (lecture && !deja_vu[i]) {
-        printf("\033[31m"); // rouge
-        printf("Erreur : aucune valeur n’a été écrite à cette adresse");
-        printf("\033[0m\n"); // réinitialiser la couleur
+        if (!quiet) {
+            printf("\033[31m"); // rouge
+            printf("Erreur : aucune valeur n’a été écrite à cette adresse");
+            printf("\033[0m\n"); // réinitialiser la couleur
+        }
         return 2;
     }
     return 0;
@@ -45,8 +55,8 @@ char verifie_case_memoire(
  * écrit dans cette case mémoire (i.e. l’élément correspondant dans deja_vu est
  * à false), affiche une erreur.
  */
-void affiche_case_memoire(int i, int* ram, bool* deja_vu, int taille) {
-    if (verifie_case_memoire(i, deja_vu, taille, true) == 0)
+void affiche_case_memoire(int i, int* ram, bool* deja_vu, int taille, bool quiet) {
+    if (verifie_case_memoire(i, deja_vu, taille, true, quiet) == 0)
         printf("%d\n", ram[i]);
 }
 
@@ -56,13 +66,14 @@ void affiche_case_memoire(int i, int* ram, bool* deja_vu, int taille) {
  * l’adresse est en-dehors de la mémoire, affiche une erreur.
  */
 void stocke_dans_case_memoire(
-    int n, int i, int* ram, bool* deja_vu, int taille
+    int n, int i, int* ram, bool* deja_vu, int taille, bool quiet
 ) {
-    if (verifie_case_memoire(i, deja_vu, taille, false) != 0)
+    if (verifie_case_memoire(i, deja_vu, taille, false, quiet) != 0)
         return;
     deja_vu[i] = true;
     ram[i] = n;
-    printf("%d est stocké à l’adresse %d.\n", n, i);
+    if (!quiet)
+        printf("%d est stocké à l’adresse %d.\n", n, i);
 }
 
 /*
@@ -73,20 +84,20 @@ void stocke_dans_case_memoire(
  * correspondant dans deja_vu est à false)
  */
 void addition(
-    int a, int b, int c, int ram[], bool deja_vu[], int taille
+    int a, int b, int c, int ram[], bool deja_vu[], int taille, int quiet
 ) {
-    if (verifie_case_memoire(a, deja_vu, taille, true) != 0)
+    if (verifie_case_memoire(a, deja_vu, taille, true, quiet) != 0)
         return;
-    if (verifie_case_memoire(b, deja_vu, taille, true) != 0)
+    if (verifie_case_memoire(b, deja_vu, taille, true, quiet) != 0)
         return;
-    if (verifie_case_memoire(c, deja_vu, taille, false) != 0)
+    if (verifie_case_memoire(c, deja_vu, taille, false, quiet) != 0)
         return;
 
     deja_vu[c] = true;
     ram[c] = ram[a] + ram[b];
 }
 
-int main() {
+int main(int argc, char *argv[]) {
     int ram[MAX_LEN];
     bool deja_vu[MAX_LEN]; // true si une valeur a déjà été écrite, false sinon
     int taille;
@@ -95,53 +106,74 @@ int main() {
     int adresse2;
     int adresse3;
 
-    printf("Choisir la taille de la mémoire : ");
+    bool quiet = (argc > 1 && strcmp(argv[1], "quiet") == 0);
+
+    if (!quiet)
+        printf("Choisir la taille de la mémoire : ");
     if (scanf("%d", &taille) == 0) {
-        printf("\033[31m"); // rouge
-        printf( "Taille invalide"); printf("\033[0m\n"); // réinitialiser la couleur
+        if (!quiet) {
+            printf("\033[31m"); // rouge
+            printf( "Taille invalide");
+            printf("\033[0m\n"); // réinitialiser la couleur
+        }
         return 1;
     }
     if (taille < 0 || taille > 1024) {
-        printf("\033[31m"); // rouge
-        printf(
-            "La taille de la mémoire doit être comprise entre 0 et 1024 inclus"
-        );
-        printf("\033[0m\n"); // réinitialiser la couleur
+        if (!quiet) {
+            printf("\033[31m"); // rouge
+            printf(
+                "La taille de la mémoire doit être comprise entre 0 et 1024 inclus"
+            );
+            printf("\033[0m\n"); // réinitialiser la couleur
+        }
         return 2;
     }
 
-    printf("Note : les adresses commencent à 0.\n");
+    if (!quiet)
+        printf("Note : les adresses commencent à 0.\n");
 
     while (true) {
-        printf("\n");
-        printf("Que voulez-vous faire ?\n");
-        printf("1. Accéder à une valeur\n");
-        printf("2. Stocker une valeur\n");
-        printf("3. Faire une addition\n");
-        printf("4. Quitter le programme\n");
-        printf("? ");
+        if (!quiet) {
+            printf("\n");
+            printf("Que voulez-vous faire ?\n");
+            printf("1. Accéder à une valeur\n");
+            printf("2. Stocker une valeur\n");
+            printf("3. Faire une addition\n");
+            printf("4. Quitter le programme\n");
+            printf("? ");
+        }
 
         if (scanf("%d", &choix) == 0)
             break;
 
         if (choix == 1) {
-            printf("Rentrez l’adresse mémoire : ");
+            if (!quiet)
+                printf("Rentrez l’adresse mémoire : ");
             if (scanf("%d", &adresse1) == 0)
                 break;
-            affiche_case_memoire(adresse1, ram, deja_vu, taille);
+
+            affiche_case_memoire(adresse1, ram, deja_vu, taille, quiet);
+
         } else if (choix == 2) {
-            printf("Rentrer l’adresse mémoire puis la valeur à stocker : ");
+            if (!quiet)
+                printf("Rentrer l’adresse mémoire puis la valeur à stocker : ");
             if (scanf("%d %d", &adresse1, &adresse2) == 0)
                 break;
-            stocke_dans_case_memoire(adresse2, adresse1, ram, deja_vu, taille);
+
+            stocke_dans_case_memoire(adresse2, adresse1, ram, deja_vu, taille, quiet);
+
         } else if (choix == 3) {
-            printf("Rentrer les deux adresses mémoire à additioner : ");
+            if (!quiet)
+                printf("Rentrer les deux adresses mémoire à additioner : ");
             if (scanf("%d %d", &adresse1, &adresse2) == 0)
                 break;
-            printf("Rentrer l’adresse mémoire à laquelle stocker le résultat : ");
+            if (!quiet)
+                printf("Rentrer l’adresse mémoire à laquelle stocker le résultat : ");
             if (scanf("%d", &adresse3) == 0)
                 break;
-            addition(adresse1, adresse2, adresse3, ram, deja_vu, taille);
+
+            addition(adresse1, adresse2, adresse3, ram, deja_vu, taille, quiet);
+
         } else {
             break;
         }
