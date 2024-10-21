72271e9b6e446b50758bb7162ff5275c81ee16bb
Jean Dubois <jd-dev@laposte.net>
2024-10-21 22:27:11 +0100

    tp3 : exercice9 : add print ascii

--- MP2I/TPs/TP03/Exercice09/programme.c
+++ MP2I/TPs/TP03/Exercice09/programme.c
@@ -61,6 +61,16 @@ void affiche_case_memoire(int i, int* ram, bool* deja_vu, int taille, bool quiet
 }
 
 /*
+ * Exactement comme affiche_case_memoire, mais sans afficher de retour-ligne
+ */
+void affiche_case_memoire_sans_retour_ligne(
+    int i, int* ram, bool* deja_vu, int taille, bool quiet
+) {
+    if (verifie_case_memoire(i, deja_vu, taille, true, quiet) == 0)
+        printf("%d", ram[i]);
+}
+
+/*
  * Stocke la valeur n à l’adresse i de la mémoire (tableau ram) de taille
  * taille. Met à jour le tableau deja_vu des cases déjà modifiées. Si
  * l’adresse est en-dehors de la mémoire, affiche une erreur.
@@ -95,6 +105,39 @@ void addition(
 
     deja_vu[c] = true;
     ram[c] = ram[a] + ram[b];
+}
+
+/*
+ * Calcule le produit des valeurs stockées dans les adresses mémoires a et b
+ * du tableau ram de taille taille, et stocke cette valeur dans la case c.
+ * Affiche une erreur si une de ces adresses est invalide (hors de la mémoire
+ * ou, dans le cas de a et b, n’a jamais été écrite ; i.e. l’élément
+ * correspondant dans deja_vu est à false)
+ */
+void multiplication(
+    int a, int b, int c, int ram[], bool deja_vu[], int taille, int quiet
+) {
+    if (verifie_case_memoire(a, deja_vu, taille, true, quiet) != 0)
+        return;
+    if (verifie_case_memoire(b, deja_vu, taille, true, quiet) != 0)
+        return;
+    if (verifie_case_memoire(c, deja_vu, taille, false, quiet) != 0)
+        return;
+
+    deja_vu[c] = true;
+    ram[c] = ram[a] * ram[b];
+}
+
+/*
+ * Affiche le caractère ASCII représenté par les 8 derniers bits de la case
+ * numéro i de la mémoire (tableau ram), de taille taille.
+ * Si la valeur est en-dehors de la mémoire ou que l’utilisateur n’a jamais
+ * écrit dans cette case mémoire (i.e. l’élément correspondant dans deja_vu est
+ * à false), affiche une erreur.
+ */
+void affiche_ascii(int i, int* ram, bool* deja_vu, int taille, bool quiet) {
+    if (verifie_case_memoire(i, deja_vu, taille, true, quiet) == 0)
+        printf("%c", (char)(ram[i]%256));
 }
 
 int main(int argc, char *argv[]) {
@@ -139,6 +182,8 @@ int main(int argc, char *argv[]) {
             printf("1. Accéder à une valeur\n");
             printf("2. Stocker une valeur\n");
             printf("3. Faire une addition\n");
+            printf("5. Faire une multiplication\n");
+            printf("6. Afficher des options supplémentaires\n");
             printf("4. Quitter le programme\n");
             printf("? ");
         }
@@ -174,6 +219,39 @@ int main(int argc, char *argv[]) {
 
             addition(adresse1, adresse2, adresse3, ram, deja_vu, taille, quiet);
 
+        } else if (choix == 5) {
+            if (!quiet)
+                printf("Rentrer les deux adresses mémoire à multiplier : ");
+            if (scanf("%d %d", &adresse1, &adresse2) == 0)
+                break;
+            if (!quiet)
+                printf("Rentrer l’adresse mémoire à laquelle stocker le résultat : ");
+            if (scanf("%d", &adresse3) == 0)
+                break;
+
+            multiplication(adresse1, adresse2, adresse3, ram, deja_vu, taille, quiet);
+
+        } else if (choix == 6) {
+            if (!quiet) {
+                printf("Choix supplémentaires :\n");
+                printf("7. Afficher une valeur sans afficher de retour-ligne\n");
+                printf("8. Afficher le caractère ASCII représenté par les 8 ");
+                printf("derniers bits du nombre stocké à une certaine adresse\n");
+            }
+        } else if (choix == 7) {
+            if (!quiet)
+                printf("Rentrez l’adresse mémoire : ");
+            if (scanf("%d", &adresse1) == 0)
+                break;
+
+            affiche_case_memoire_sans_retour_ligne(adresse1, ram, deja_vu, taille, quiet);
+        } else if (choix == 8) {
+            if (!quiet)
+                printf("Rentrez l’adresse mémoire : ");
+            if (scanf("%d", &adresse1) == 0)
+                break;
+
+            affiche_ascii(adresse1, ram, deja_vu, taille, quiet);
         } else {
             break;
         }
