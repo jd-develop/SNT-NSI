6a4d23db12537671449f4f964894edf32fc6d002
Jean Dubois <jd-dev@laposte.net>
2024-10-22 20:13:00 +0100

    exo9: add tests and rename examples

--- MP2I/TPs/TP03/Exercice09/programme.c
+++ MP2I/TPs/TP03/Exercice09/programme.c
@@ -2,12 +2,13 @@
  * Simule une petite mémoire RAM. Ne peut stocker que des entiers. Permet
  * d’accéder à une valeur à partir de son adresse mémoire, de stocker une
  * valeur à une adresse mémoire, de faire des additions, des multiplications,
- * des modulos, mais aussi d’afficher des caractères ASCII, …
+ * des modulos, mais aussi d’afficher des caractères ASCII, de n’exécuter des
+ * instructions que sous certaines conditions…
  *
  * Un mode silencieux est disponible en passant l’argument « quiet » dans la
  * ligne de commande : pratique pour écrire des programmes en « langage
- * machine » puis de les envoyer dans l’entrée standard :) – cf. exemple
- * « fibo » (cat fibo | ./a.out quiet)
+ * machine » puis de les envoyer dans l’entrée standard :) – cf. exemples
+ * (par exemple pour « fibo » : `cat examples/fibo | ./a.out quiet`)
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -51,6 +52,20 @@ char verifie_case_memoire(
 }
 
 /*
+ * Teste la fonction verifie_case_memoire
+ */
+void tests_verifie_case_memoire() {
+    bool deja_vu[6] = {true, false, false, true, true, false};
+    assert(verifie_case_memoire(1, deja_vu, 6, false, true) == 0);
+    assert(verifie_case_memoire(0, deja_vu, 6, false, true) == 0);
+    assert(verifie_case_memoire(-7, deja_vu, 6, false, true) == 1);
+    assert(verifie_case_memoire(6, deja_vu, 6, false, true) == 1);
+    assert(verifie_case_memoire(6, deja_vu, 6, true, true) == 1);
+    assert(verifie_case_memoire(5, deja_vu, 6, true, true) == 2);
+    assert(verifie_case_memoire(4, deja_vu, 6, true, true) == 0);
+}
+
+/*
  * Affiche la case numéro i de la mémoire (tableau ram), de taille taille.
  * Si la valeur est en-dehors de la mémoire ou que l’utilisateur n’a jamais
  * écrit dans cette case mémoire (i.e. l’élément correspondant dans deja_vu est
@@ -85,6 +100,33 @@ bool case_memoire_egale_zero(int i, int* ram, bool* deja_vu, int taille, bool qu
 }
 
 /*
+ * Teste la fonction case_memoire_egale_zero
+ */
+void tests_case_memoire_egale_zero() {
+    int ram[6] = {-6, 0, 4, -9, 0, 7};
+    bool deja_vu1[6] = {true, false, false, true, true, false};
+    bool deja_vu2[6] = {true, true, true, true, true, true};
+
+    assert(!case_memoire_egale_zero(-1, ram, deja_vu1, 6, true));
+    assert(!case_memoire_egale_zero(0, ram, deja_vu1, 6, true));
+    assert(!case_memoire_egale_zero(1, ram, deja_vu1, 6, true));
+    assert(!case_memoire_egale_zero(2, ram, deja_vu1, 6, true));
+    assert(!case_memoire_egale_zero(3, ram, deja_vu1, 6, true));
+    assert(case_memoire_egale_zero(4, ram, deja_vu1, 6, true));
+    assert(!case_memoire_egale_zero(5, ram, deja_vu1, 6, true));
+    assert(!case_memoire_egale_zero(6, ram, deja_vu1, 6, true));
+
+    assert(!case_memoire_egale_zero(-1, ram, deja_vu2, 6, true));
+    assert(!case_memoire_egale_zero(0, ram, deja_vu2, 6, true));
+    assert(case_memoire_egale_zero(1, ram, deja_vu2, 6, true));
+    assert(!case_memoire_egale_zero(2, ram, deja_vu2, 6, true));
+    assert(!case_memoire_egale_zero(3, ram, deja_vu2, 6, true));
+    assert(case_memoire_egale_zero(4, ram, deja_vu2, 6, true));
+    assert(!case_memoire_egale_zero(5, ram, deja_vu2, 6, true));
+    assert(!case_memoire_egale_zero(6, ram, deja_vu1, 6, true));
+}
+
+/*
  * Renvoie true si la case i de la mémoire (tableau ram), de taille taille, est
  * ⩽ 0, false sinon ou si une erreur est survenue (par exemple si la
  * valeur est en-dehors de la mémoire ou que l’utilisateur n’a jamais écrit
@@ -95,6 +137,33 @@ bool case_memoire_negative_ou_nulle(int i, int* ram, bool* deja_vu, int taille, 
     if (verifie_case_memoire(i, deja_vu, taille, true, quiet) != 0)
         return false;
     return (ram[i] <= 0);
+}
+
+/*
+ * Teste la fonction case_memoire_negative_ou_nulle
+ */
+void tests_case_memoire_negative_ou_nulle() {
+    int ram[6] = {-6, 0, 4, -9, 0, 7};
+    bool deja_vu1[6] = {true, false, true, false, true, false};
+    bool deja_vu2[6] = {true, true, true, true, true, true};
+
+    assert(!case_memoire_negative_ou_nulle(-1, ram, deja_vu1, 6, true));
+    assert(case_memoire_negative_ou_nulle(0, ram, deja_vu1, 6, true));
+    assert(!case_memoire_negative_ou_nulle(1, ram, deja_vu1, 6, true));
+    assert(!case_memoire_negative_ou_nulle(2, ram, deja_vu1, 6, true));
+    assert(!case_memoire_negative_ou_nulle(3, ram, deja_vu1, 6, true));
+    assert(case_memoire_negative_ou_nulle(4, ram, deja_vu1, 6, true));
+    assert(!case_memoire_negative_ou_nulle(5, ram, deja_vu1, 6, true));
+    assert(!case_memoire_negative_ou_nulle(6, ram, deja_vu1, 6, true));
+
+    assert(!case_memoire_negative_ou_nulle(-1, ram, deja_vu2, 6, true));
+    assert(case_memoire_negative_ou_nulle(0, ram, deja_vu2, 6, true));
+    assert(case_memoire_negative_ou_nulle(1, ram, deja_vu2, 6, true));
+    assert(!case_memoire_negative_ou_nulle(2, ram, deja_vu2, 6, true));
+    assert(case_memoire_negative_ou_nulle(3, ram, deja_vu2, 6, true));
+    assert(case_memoire_negative_ou_nulle(4, ram, deja_vu2, 6, true));
+    assert(!case_memoire_negative_ou_nulle(5, ram, deja_vu2, 6, true));
+    assert(!case_memoire_negative_ou_nulle(6, ram, deja_vu1, 6, true));
 }
 
 /*
@@ -111,6 +180,28 @@ void stocke_dans_case_memoire(
     ram[i] = n;
     if (!quiet)
         printf("%d est stocké à l’adresse %d.\n", n, i);
+}
+
+/*
+ * Teste la fonction stocke_dans_case_memoire
+ */
+void tests_stocke_dans_case_memoire() {
+    int ram[6] = {-6, 0, 4, -9, 0, 7};
+    bool deja_vu[6] = {true, false, true, false, true, false};
+
+    stocke_dans_case_memoire(8, -1, ram, deja_vu, 6, true);
+    stocke_dans_case_memoire(10, 0, ram, deja_vu, 6, true);
+    stocke_dans_case_memoire(12, 2, ram, deja_vu, 6, true);
+    stocke_dans_case_memoire(15, 5, ram, deja_vu, 6, true);
+    stocke_dans_case_memoire(17, 6, ram, deja_vu, 6, true);
+    stocke_dans_case_memoire(19, 565, ram, deja_vu, 6, true);
+
+    assert(ram[0] == 10);
+    assert(ram[1] == 0);
+    assert(ram[2] == 12);
+    assert(ram[3] == -9);
+    assert(ram[4] == 0);
+    assert(ram[5] == 15);
 }
 
 /*
@@ -135,6 +226,31 @@ void addition(
 }
 
 /*
+ * Teste la fonction addition
+ */
+void tests_addition() {
+    int ram[6] = {-6, 0, 4, -9, 0, 7};
+    bool deja_vu[6] = {true, false, true, false, true, false};
+
+    addition(0, 2, 1, ram, deja_vu, 6, true);
+    assert(ram[0] == -6);
+    assert(ram[2] == 4);
+    assert(ram[1] == -2);
+    assert(deja_vu[0]);
+    assert(deja_vu[1]);
+    assert(deja_vu[2]);
+
+    addition(0, 3, 1, ram, deja_vu, 6, true);
+    assert(ram[0] == -6);
+    assert(ram[3] == -9);
+    assert(ram[1] == -2);
+    assert(deja_vu[0]);
+    assert(deja_vu[1]);
+    assert(deja_vu[2]);
+    assert(!deja_vu[3]);
+}
+
+/*
  * Calcule le produit des valeurs stockées dans les adresses mémoires a et b
  * du tableau ram de taille taille, et stocke cette valeur dans la case c.
  * Affiche une erreur si une de ces adresses est invalide (hors de la mémoire
@@ -156,6 +272,31 @@ void multiplication(
 }
 
 /*
+ * Teste la fonction multiplication
+ */
+void tests_multiplication() {
+    int ram[6] = {-6, 0, 4, -9, 0, 7};
+    bool deja_vu[6] = {true, false, true, false, true, false};
+
+    multiplication(0, 2, 1, ram, deja_vu, 6, true);
+    assert(ram[0] == -6);
+    assert(ram[2] == 4);
+    assert(ram[1] == -24);
+    assert(deja_vu[0]);
+    assert(deja_vu[1]);
+    assert(deja_vu[2]);
+
+    multiplication(0, 3, 1, ram, deja_vu, 6, true);
+    assert(ram[0] == -6);
+    assert(ram[3] == -9);
+    assert(ram[1] == -24);
+    assert(deja_vu[0]);
+    assert(deja_vu[1]);
+    assert(deja_vu[2]);
+    assert(!deja_vu[3]);
+}
+
+/*
  * Calcule le modulo des valeurs stockées dans les adresses mémoires a et b
  * du tableau ram de taille taille, et stocke cette valeur dans la case c.
  * Affiche une erreur si une de ces adresses est invalide (hors de la mémoire
@@ -174,6 +315,31 @@ void modulo(
 
     deja_vu[c] = true;
     ram[c] = ram[a] % ram[b];
+}
+
+/*
+ * Teste la fonction modulo
+ */
+void tests_modulo() {
+    int ram[6] = {-6, 0, 4, -9, 0, 7};
+    bool deja_vu[6] = {true, false, true, false, true, false};
+
+    modulo(0, 2, 1, ram, deja_vu, 6, true);
+    assert(ram[0] == -6);
+    assert(ram[2] == 4);
+    assert(ram[1] == -2);
+    assert(deja_vu[0]);
+    assert(deja_vu[1]);
+    assert(deja_vu[2]);
+
+    modulo(0, 3, 1, ram, deja_vu, 6, true);
+    assert(ram[0] == -6);
+    assert(ram[3] == -9);
+    assert(ram[1] == -2);
+    assert(deja_vu[0]);
+    assert(deja_vu[1]);
+    assert(deja_vu[2]);
+    assert(!deja_vu[3]);
 }
 
 /*
@@ -199,6 +365,31 @@ void quotient(
 }
 
 /*
+ * Teste la fonction quotient
+ */
+void tests_quotient() {
+    int ram[6] = {-6, 0, 4, -9, 0, 7};
+    bool deja_vu[6] = {true, false, true, false, true, false};
+
+    quotient(0, 2, 1, ram, deja_vu, 6, true);
+    assert(ram[0] == -6);
+    assert(ram[2] == 4);
+    assert(ram[1] == -1);
+    assert(deja_vu[0]);
+    assert(deja_vu[1]);
+    assert(deja_vu[2]);
+
+    quotient(0, 3, 1, ram, deja_vu, 6, true);
+    assert(ram[0] == -6);
+    assert(ram[3] == -9);
+    assert(ram[1] == -1);
+    assert(deja_vu[0]);
+    assert(deja_vu[1]);
+    assert(deja_vu[2]);
+    assert(!deja_vu[3]);
+}
+
+/*
  * Affiche le caractère ASCII représenté par les 8 derniers bits de la case
  * numéro i de la mémoire (tableau ram), de taille taille.
  * Si la valeur est en-dehors de la mémoire ou que l’utilisateur n’a jamais
@@ -211,6 +402,17 @@ void affiche_ascii(int i, int* ram, bool* deja_vu, int taille, bool quiet) {
 }
 
 int main(int argc, char *argv[]) {
+    /* TESTS */
+    tests_verifie_case_memoire();
+    tests_case_memoire_egale_zero();
+    tests_case_memoire_negative_ou_nulle();
+    tests_stocke_dans_case_memoire();
+    tests_addition();
+    tests_multiplication();
+    tests_modulo();
+    tests_quotient();
+    /* ----- */
+
     int ram[MAX_LEN];
     bool deja_vu[MAX_LEN]; // true si une valeur a déjà été écrite, false sinon
     int taille;
