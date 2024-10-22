3e997c9976c7f958aac49a5be8b7d774701bb3bf
Jean Dubois <jd-dev@laposte.net>
2024-10-22 20:25:11 +0100

    add exo6

--- MP2I/TPs/TP03/Exercice06/programme.c
+++ MP2I/TPs/TP03/Exercice06/programme.c
@@ -1,10 +1,40 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <assert.h>
 
-/* Lit un entier dans le terminal. Si la lecture réussit, renvoie true et
- * stocke l’entier lu dans res. Sinon, renvoie false.
- * Dans les deux cas, l’entrée du terminal est ensuite vidée de tout caractère
- * additionnel */
 
-int main() {
-    return 0;
+/*
+ * Lit un entier dans le terminal.
+ * * Si la lecture réussit, alors la fonction renvoie true et stocke l'entier lu
+ *   dans res.
+ * * Si la lecture est infructueuse, alors la fonction renvoie false.
+ *
+ * Dans les deux cas, l'entrée du terminal est ensuite vidée de tout caractère
+ * additionnel
+ */
+bool read_int_and_flush(int* res){
+    assert(res != NULL);
+    int x;
+    char non_int;
+    if (scanf("%d", &x) != 1)
+        return false;
+    do {
+        if (scanf("%c", &non_int) != 1)
+            break;
+    } while (non_int != '\n');
+    *res = x;
+    return true;
 }
+
+int main(){
+    int x = 1;
+    do {
+        printf("Rentrez un entier (0 pour finir): ");
+        bool b = read_int_and_flush(&x);
+        if (b)
+            printf("Entier lu: %d\n", x);
+        else
+            printf("Erreur de lecture\n");
+    } while (x != 0);
+}
+
