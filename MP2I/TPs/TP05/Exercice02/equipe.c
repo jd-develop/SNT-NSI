b769292df2b664986c97fe66d62110efe688e9fd
jd-develop <jd-dev@laposte.net>
2024-11-24 17:54:39 +0100

    [NOT WORKING] continue exercice 2

--- MP2I/TPs/TP05/Exercice02/equipe.c
+++ MP2I/TPs/TP05/Exercice02/equipe.c
@@ -36,6 +36,7 @@ void print_equipe(equipe_t* e) {
 
 /* Libère la mémoire réservée à un étudiant */
 void free_etu(etu_t* e) {
+    free(e->prenom);
     free(e);
 }
 
@@ -48,22 +49,67 @@ void free_equipe(equipe_t* e) {
     free(e);
 }
 
+/* Rajoute les membres de l’équipe pointée par `src` à l’équipe pointée par
+ * `dst`. Garde le capitaine de l’équipe la plus grande. Les structures
+ * etudiant manipulées sont dupliquées et l’équipe source n’est pas
+ * invalidée. Il faut donc, après exécution de cette fonction, libérer
+ * la mémoire allouée aux deux équipes
+ */
+void agrandir_equipe(equipe_t* dst, equipe_t* src) {
+    uint nb_membres_tot = dst->nb_membres + src->nb_membres
+    uint indice_cpt;
+    if (dst->nb_membres > src->nb_membres) {
+        indice_cpt = dst->indice_cpt;
+    } else {
+        indice_cpt = dst->nb_membres + src->indice_cpt;
+    }
+
+    etu_t** nouveaux_membres = malloc(nb_membres_tot*sizeof(etu_t*))
+
+    // on copie les poiteurs des étudiants de l’équipe destination vers la
+    // nouvelle équipe destination
+    for (uint i = 0; i < dst->nb_membres; i++) {
+        nouveaux_membres[i] = dst->membres[i];
+    }
+    free(dst->membres);
+    dst->membres = nouveaux_membres;
+
+    etu_t* nouveau_membre;
+    int longueur_prenom;
+    for (uint i = 0; i < src->nb_membres; i++) {
+        nouveau_membre = malloc(sizeof(etu_t));
+
+        longueur_prenom = strlen(src->membres[i]->prenom) + 1;
+        nouveau_membre->prenom = malloc(longueur_prenom*sizeof(char));
+        strcpy(nouveau_membre->prenom, src->membres[i]);
+
+        nouveau_membre.age = src->membres[i]->age;
+
+        dst->membres[dst->nb_membres + i] = nouveau_membre;
+    }
+}
+
+
 int main() {
     equipe_t* equipe_1 = malloc(sizeof(equipe_t));
     equipe_1->membres = malloc(3*sizeof(etu_t*));
     equipe_1->nb_membres = 3;
 
     equipe_1->membres[0] = malloc(sizeof(etu_t));
-    equipe_1->membres[0]->prenom = "Camille";
+    equipe_1->membres[0]->prenom = malloc(8*sizeof(char));
+    strcpy(equipe_1->membres[0]->prenom, "Camille");
+
     equipe_1->membres[0]->age = 23;
 
     equipe_1->membres[1] = malloc(sizeof(etu_t));
-    equipe_1->membres[1]->prenom = "Leila";
+    equipe_1->membres[1]->prenom = malloc(6*sizeof(char));
+    strcpy(equipe_1->membres[1]->prenom, "Leila");
     equipe_1->membres[1]->age = 20;
     equipe_1->indice_cpt = 1;
 
     equipe_1->membres[2] = malloc(sizeof(etu_t));
-    equipe_1->membres[2]->prenom = "Thibault";
+    equipe_1->membres[2]->prenom = malloc(9*sizeof(char));
+    strcpy(equipe_1->membres[2]->prenom, "Thibault");
     equipe_1->membres[2]->age = 22;
 
     print_equipe(equipe_1);
