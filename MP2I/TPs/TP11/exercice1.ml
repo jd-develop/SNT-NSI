ceeb42ef3c4af1483a1bba1c285b48d724e0bb32
Jean Dubois <jd-dev@laposte.net>
2025-03-12 16:43:08 +0100

    tp11: finish the non-optional part of exercice1

--- MP2I/TPs/TP11/exercice1.ml
+++ MP2I/TPs/TP11/exercice1.ml
@@ -11,25 +11,53 @@ let t = N(3,
   ),
   N(7, V, V))
 
+let t2 = N(1,
+  N(2,
+    N(4, V, V),
+    N(5,
+      N(6, V, V),
+      N(7, N(8, V, V), V))),
+  N(3, V,
+    N(9, V,
+      N(10,
+        N(11, V, V),
+        N(12, V, V)))))
+
+let t3 = N(2,
+  N(3,
+    N(5, V, V),
+    N(6,
+      N(7, V, V),
+      N(8, N(9, V, V), V))),
+  N(4, V,
+    N(10, V,
+      N(11,
+        N(12, V, V),
+        N(13, V, V)))))
+
 (* `taille a` renvoie le nombre total de nœuds de a *)
 let rec taille (a: 'a ab) : int = match a with
   | V -> 0
   | N(x, g, d) -> 1 + taille g + taille d
 
-let test_taille () =
-  assert (taille V = 0);
-  assert (taille t = 4);
-  assert (taille (N(2, t, t)) = 9)
+
+let test_taille (taille_func: 'a ab -> int) =
+  assert (taille_func V = 0);
+  assert (taille_func t = 4);
+  assert (taille_func (N(2, t, t)) = 9)
+
 
 (* Calcule la hauteur de `a` *)
 let rec hauteur (a: 'a ab) : int = match a with
   | V -> -1
   | N(x, g, d) -> 1 + max (hauteur g) (hauteur d)
 
+
 let test_hauteur () =
   assert (taille V = 0);
   assert (taille t = 4);
   assert (taille (N(2, t, t)) = 9)
+
 
 (* Renvoie true si `a` est une feuille, i.e. si ses deux enfants
  * sont vides
@@ -37,6 +65,7 @@ let test_hauteur () =
 let est_feuille (a: 'a ab) : bool = match a with
   | N(x, V, V) -> true
   | _ -> false
+
 
 let test_est_feuille () =
   assert (not (est_feuille V));
@@ -46,11 +75,13 @@ let test_est_feuille () =
   assert (not (est_feuille t));
   assert (est_feuille (N(2, V, V)))
 
+
 (* Renvoie le nombre de feuilles de `a` *)
 let rec feuilles (a: 'a ab) : int = match a with
   | V -> 0
   | N(x, V, V) -> 1
   | N(x, g, v) -> feuilles g + feuilles v
+
 
 let test_feuilles () =
   assert (feuilles V = 0);
@@ -58,6 +89,7 @@ let test_feuilles () =
   assert (feuilles t = 2);
   assert (feuilles (N(1, t, V)) = 2);
   assert (feuilles (N(1, t, t)) = 4)
+
 
 (* Renvoie l’étiquette du nœud correspondant au chemin `c` dans
  * l’arbre `a` (où true désigne la gauche et false la droite).
@@ -68,6 +100,7 @@ let rec etiquette (a: 'a ab) (c: bool list) : 'a = match c, a with
   | [], N(x, _, _) -> x
   | x::q, N(_, g, d) -> if x then etiquette g q
                         else etiquette d q
+
 
 let test_etiquette () =
   assert (etiquette (N(1, V, V)) [] = 1);
@@ -89,6 +122,7 @@ let test_etiquette () =
   assert (etiquette (N(1, V, t)) [false;true; false] = 8);
   assert (etiquette (N(1, V, t)) [false;false] = 7)
 
+
 (* Renvoie l’étiquette du nœud correspondant au chemin `c` dans
  * l’arbre `a` (où true désigne la gauche et false la droite).
  * Renvoie None en cas de chemin invalide.
@@ -98,6 +132,7 @@ let rec etiquette_opt (a: 'a ab) (c: bool list) : 'a option = match c, a with
   | [], N(x, _, _) -> Some x
   | x::q, N(_, g, d) -> if x then etiquette_opt g q
                         else etiquette_opt d q
+
 
 let test_etiquette_opt () =
   assert (etiquette_opt V [] = None);
@@ -129,11 +164,97 @@ let test_etiquette_opt () =
   assert (etiquette_opt (N(1, V, t)) [false;true; false] = Some 8);
   assert (etiquette_opt (N(1, V, t)) [false;false] = Some 7)
 
+
+(* Affiche les étiquettes de l’arbre binaire d’entiers `a` dans l’ordre
+ * _préfixe_
+ *)
+let rec print_prefixe (a: int ab) : unit = match a with
+  | V -> ()
+  | N(x, g, d) -> print_int x; print_string " "; print_prefixe g; print_prefixe d
+
+
+(* Affiche les étiquettes de l’arbre binaire d’entiers `a` dans l’ordre
+ * _postfixe_
+ *)
+let rec print_postfixe (a: int ab) : unit = match a with
+  | V -> ()
+  | N(x, g, d) -> print_postfixe g; print_postfixe d; print_int x; print_string " "
+
+
+(* Affiche les étiquettes de l’arbre binaire d’entiers `a` dans l’ordre
+ * _infixe_
+ *)
+let rec print_infixe (a: int ab) : unit = match a with
+  | V -> ()
+  | N(x, g, d) -> print_infixe g; print_int x; print_string " "; print_infixe d
+
+
+(* Calcule la taille de a et lui ajoute n *)
+let rec taille_add (a: int ab) (n: int) : int = match a with
+  | V -> n
+  | N(x, g, d) -> let ng = taille_add g (n+1) in taille_add d ng
+
+
+(* Calcule la taille de a *)
+let taille2 (a: int ab) = taille_add a 0
+
+
+(* Renvoie la liste des éléments de `a` dans l’ordre postfixe *)
+let liste_postfixe (a: 'a ab) : 'a list =
+  (* Renvoie (liste_postfixe a)@l *)
+  let rec postfixe_concat (a: 'a ab) (l: 'a list) : 'a list = match a with
+    | V -> l
+    | N(x, g, d) -> let ld = postfixe_concat d (x::l) in (postfixe_concat g ld)
+  in postfixe_concat a []
+
+
+let test_liste_postfixe () =
+  assert (liste_postfixe V = []);
+  assert (liste_postfixe (N(1, V, V)) = [1]);
+  assert (liste_postfixe (N(1, N(2, V, V), V)) = [2; 1]);
+  assert (liste_postfixe (N(1, N(2, V, V), N(3, V, V))) = [2; 3; 1]);
+  assert (liste_postfixe t2 = [4; 6; 8; 7; 5; 2; 11; 12; 10; 9; 3; 1])
+
+
+(* applique la fonction f à chaque nœud de l’arbre a et renvoie l’arbre
+ * qui en résulte
+ *)
+let rec tree_map (f: 'a -> 'b) (a: 'a ab) : 'b ab = match a with
+  | V -> V
+  | N(x, g, d) -> N(f x, tree_map f g, tree_map f d)
+
+
+let test_tree_map () =
+  assert (tree_map taille2 V = V);
+  assert (tree_map (fun x -> x) t = t);
+  assert (tree_map (fun x -> x) t2 = t2);
+  assert (tree_map (fun x -> x+1) t2 = t3);
+  assert (tree_map (fun x -> x-1) t3 = t2)
+
+
+(* Calule la somme de tous les entiers contenus dans `a` *)
+let rec tree_sum (a: int ab) : int = match a with
+  | V -> 0
+  | N(x, g, d) -> x + tree_sum g + tree_sum d
+
+
+let test_tree_sum () =
+  assert (tree_sum V = 0);
+  assert (tree_sum t = 23);
+  assert (tree_sum t2 = 78);
+  assert (tree_sum t3 = 90);
+  assert (tree_sum t3 = tree_sum t2 + taille2 t2)
+
+
 let test () =
-  test_taille ();
+  test_taille taille;
+  test_taille taille2;
   test_hauteur ();
   test_est_feuille ();
   test_feuilles ();
   test_etiquette ();
-  test_etiquette_opt ()
+  test_etiquette_opt ();
+  test_liste_postfixe ();
+  test_tree_map ();
+  test_tree_sum ()
 
