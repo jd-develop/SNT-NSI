c83531084a86d0445c89602154654b779ff69e80
Jean Dubois <jd-dev@laposte.net>
2025-04-24 19:30:18 +0100

    DM3: finish utils.c and utils.h

--- MP2I/DMs/DM3/problemes/utils.c
+++ MP2I/DMs/DM3/problemes/utils.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #include "utils.h"
 
 
-char* au_moins_une(char** l, int n) {
+char* join(char** l, int n, char* s, int j) {
     char* res = strdup("(");
+    bool not = (0 <= j) && (j < n);
     for (int i = 0; i < n; i++) {
+        if (not && i != j) {
+            res = realloc(res, strlen(res)+2);
+            strcat(res, "~");
+        }
         res = realloc(res, strlen(res)+strlen(l[i])+1);
         strcat(res, l[i]);
 
         if (i+1 != n) {
-            res = realloc(res, strlen(res)+4);
-            strcat(res, " | ");
+            res = realloc(res, strlen(res)+strlen(s)+1);
+            strcat(res, s);
         }
     }
     res = realloc(res, strlen(res)+2);
@@ -21,9 +27,32 @@ char* au_moins_une(char** l, int n) {
 }
 
 
+char* au_moins_une(char** l, int n) {
+    return join(l, n, " | ", -1);
+}
+
+
+char* toutes(char** l, int n, int j) {
+    return join(l, n, " & ", j);
+}
+
+
+char* au_plus_une(char** l, int n) {
+    char** l2 = malloc(sizeof(l));
+    for (int i = 0; i < n; i++) {
+        l2[i] = toutes(l, n, i);
+    }
+    return au_moins_une(l2, n);
+}
+
+
 int main () {
     char* forms[3] = {"(x & ~y)", "y", "z"};
     char* phi = au_moins_une(forms, 3);
     printf("%s\n", phi);
+    char* psi = toutes(forms, 3, -1);
+    printf("%s\n", psi);
+    char* khi = au_plus_une(forms, 3);
+    printf("%s\n", khi);
     return 0;
 }
--- MP2I/DMs/DM3/problemes/utils.h
+++ MP2I/DMs/DM3/problemes/utils.h
@@ -2,6 +2,15 @@
 #define UTILS_H
 
 /*
+ * Renvoie la concaténation des chaînes de l (tableau de taille n) avec pour
+ * séparateur la chaîne s, avec une parenthèse ouvrante et une parenthèse
+ * fermante comme premier et dernier caractères.
+ * Si j est compris entre 0 inclus et n exclus, toutes les chaînes sont
+ * précédées de "~" sauf la chaîne d’indice j
+ */
+char* join(char** l, int n, char* s, int j);
+
+/*
  * Si l est une liste de n formules supposées atomiques (i.e. sans opérateurs
  * binaires en-dehors de parenthèses), renvoie une chaîne (dans le tas)
  * contenant une formule exprimant « au moins une des formules de l est vraie ».
@@ -9,5 +18,29 @@
  * "((x & ~y) | y | z)"
  */
 char* au_moins_une(char** l, int n);
+
+/*
+ * Si l est une liste de n formules supposées atomiques (i.e. sans opérateurs
+ * binaires en-dehors de parenthèses), renvoie une chaîne (dans le tas)
+ * contenant une formule exprimant « toutes les formules de l sont vraies ».
+ * Par exemple, toutes(["(x & ~y)", "y", "z"], 3, -1) pointe vers la chaîne
+ * "((x & ~y) & y & z)"
+ *
+ * Si j est compris entre 0 inclus et n exclus, la fomule représentée
+ * par la chaîne de retour est « aucune formule n’est vraie sauf celle à
+ * l’indice j ».
+ * Par exemple, toutes(["(x & ~y)", "y", "z"], 3, 1) pointe vers la chaîne
+ * "(~(x & ~y) & y & ~z)"
+ */
+char* toutes(char** l, int n, int j);
+
+/*
+ * Si l est une liste de n formules supposées atomiques (i.e. sans opérateurs
+ * binaires en-dehors de parenthèses), renvoie une chaîne (dans le tas)
+ * contenant une formule exprimant « au plus une des formules de l est vraie ».
+ * Par exemple, au_plus_une(["(x & ~y)", "y", "z"], 3) pointe vers la chaîne
+ * "(((x & ~y) & ~y & ~z) | (~(x & ~y) & y & ~z) | (~(x & ~y) & ~y & z))"
+ */
+char* au_plus_une(char** l, int n);
 
 #endif
